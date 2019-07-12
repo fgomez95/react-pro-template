919b8b1f3d348631a24f5dcb96550d72022d6569
Fernando Gomez <fgomez.sierra95@gmail.com>
2019-07-12 09:17:01 -0600

    integrate enzyme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-	Hello, World
-    </div>
-  );
-}
+  return <div className="App">Hello, World</div>;
+};
 
 export default App;
