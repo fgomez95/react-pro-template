2491a164d0cfc730fde269fa9bec4262f958b1a1
Fernando G <fgomez@masterdevel.com>
2019-07-16 07:49:21 -0600

    update folder structure

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function Login(): JSX.Element {
+  return (
+    <div>
+      <h4>Login</h4>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Route } from "react-router-dom";
 import Home from "./Home/Home";
 import Login from "./Login/Login";
-import Dash from "./Dash/Dash";
+import Dashboard from "./Dashboard/Dashboard";
 import Navigation from "../components/UI/Navigation/Navigation";
 
 export interface MainProps {
